c0750c49b9351c0adb0b0f875f3f8340ba5afe63
Daninuro <daninuro96@gmail.com>
2023-10-01 11:38:38 +0200

    Añado Ejercicio 1

---
+++ Ejercicio1 UD03/src/main/ComLinea.java
@@ -0,0 +1,14 @@
+package main;
+
+public class ComLinea {
+
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+		
+	}
+
+}
+
